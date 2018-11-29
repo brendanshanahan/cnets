a817101e1c0512a006458f055b2ea76aef065796
James <smittyprz94@gmail.com>
2018-11-29 17:46:58 -0500

    Added fft_peaks function: (simulations.simulations.fft_peaks) Allows fft and peaks on that transform to be found. Not sure if there is convergence issue, or misunderstanding of sudo code, but cluster groups not fully found yet. Also added .gitignore

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -14,12 +14,14 @@ def relax(graph, node, laplacian, t):
     for i in neighbors:
         sum += laplacian[i]
 
+
 def draw_graph(graph):
     pos = nx.spring_layout(graph)
     # weights = [graph[u][v]['weight'] for u, v in graph.edges]
     nx.draw_networkx_edges(graph, pos, edges=graph.edges())
     nx.draw_networkx_nodes(graph, pos, node_size=20)
     plt.show()
+
 
 def wave_equation(graph, n, c=0.1):
     """
@@ -49,9 +51,55 @@ def wave_equation(graph, n, c=0.1):
 
     for t in relaxation:
         # sum over all neighbors
-        neighbors = np.dot((2*np.identity(len(laplacian)) - (c**2)*laplacian), graph.state[t+1])
+        neighbors = np.dot((2 * np.identity(len(laplacian)) - (c**2) * laplacian), graph.state[t + 1])
 
         # t-1, t-2
         relaxation = - graph.state[t]
 
         graph.update_state([relaxation + neighbors])
+
+
+def fft_peaks(graph):
+
+    try:
+        assert graph.state is not None
+    except AssertionError:
+        print('Need graph relaxation information for fft')
+        return
+
+    fft_relax = np.fft.fft(graph.state, axis=0)
+    # Get rid of 0 frequency value
+    fft_relax = fft_relax[1:]
+
+    # Empty initial conditions
+    pr_increasing_nodes = np.array(-1)
+    peaks_array = np.zeros_like(fft_relax)
+
+    dict_peak = {}
+
+    for maxcheck in range(fft_relax.shape[0])[1:]:
+        # Array of t - 1 values
+        prior_value = fft_relax[maxcheck - 1, :]
+        current_value = fft_relax[maxcheck, :]
+
+        # presently decreasing values
+        decreasing_nodes = np.where(prior_value > current_value)[0]
+        peak_nodes = np.intersect1d(pr_increasing_nodes, decreasing_nodes)
+
+        for peaks in peak_nodes:
+            # Saving peak values for later
+            peak_value = fft_relax[maxcheck - 1, peaks]
+            peaks_array[maxcheck - 1, peaks] = peak_value
+            try:
+                assert type(dict_peak[peaks]) is list
+            except KeyError:
+                dict_peak[peaks] = []
+
+            if peak_value > 0:
+                coeff = 1
+            else:
+                coeff = 0
+            dict_peak[peaks].append(coeff)
+
+        pr_increasing_nodes = np.where(prior_value < current_value)[0]
+    return fft_relax, peaks_array, dict_peak
