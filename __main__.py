0e8b68a553a0701360027463f794415c8e39aab3
James Smith <smittyprz94@gmail.com>
2019-04-18 11:38:23 -0400

    First statistical testing of sparse graphs.

--- __main__.py
+++ __main__.py
@@ -5,9 +5,9 @@ import pickle
 import os
 from pathlib import Path
 
-def gen_strong_graph(seed, init_size=200, lower=15, upper=100):
+def gen_strong_graph(seed, init_size=200, p=0.1, lower=15, upper=100):
     """TBA."""
-    graph = nx.erdos_renyi_graph(init_size, 0.01, seed=seed, directed=True)
+    graph = nx.erdos_renyi_graph(init_size, p, seed=seed, directed=True)
     strong_components = nx.algorithms.components.strongly_connected_components(graph)
 
     # Select components based on thresholds of acceptance
@@ -23,55 +23,65 @@ def gen_strong_graph(seed, init_size=200, lower=15, upper=100):
         new_map = [n for n in range(len(old_map))]
         mapping = {old_map[x]: new_map[x] for x in range(len(new_map))}
         output = nx.relabel_nodes(un_org_graph, mapping)
-        nx.draw(output)
-        plt.show()
+        # nx.draw(output)
+        # plt.show()
         return output
 
 def plot_from_dict(dict_in):
+    conductance_list = []
+    sp_list = []
     for seed in dict_in.keys():
-        iterations = dict_in[seed]['Convergence']
+        conductance_list.append(dict_in[seed]['Simulation Information']['Final Outcome']['Conductance'])
+        sp_list.append(dict_in[seed]['Sparsity'])
+        iterations = dict_in[seed]['Simulation Information']['Convergence']
         plot_array = np.zeros(len(iterations.keys()))
         for key in iterations.keys():
             plot_array[int(key.strip('Iteration-')) - 1] = iterations[key]['Conductance']
         
-        spec_const = np.ones(len(iterations.keys())) * dict_in[seed]['Spectral Const.']
-        print(spec_const)
+        spec_const = np.ones(len(iterations.keys())) * dict_in[seed]['Simulation Information']['Spectral Const.']
+        # print(spec_const)
         # plt.semilogy(spec_const)
-        plt.plot(plot_array)
-        plt.figure()
-        plt.plot(dict_in[seed]['Debug'], 'k--')
-        plt.show()
+        # plt.plot(plot_array)
+        # plt.figure()
+        # plt.plot(dict_in[seed]['Debug'], 'k--')
+    print(conductance_list)
+    plt.figure()
+    plt.plot(sp_list, conductance_list, 'ko')
+    plt.show()
 
 
 if __name__ == '__main__':
     main_dict = {}
 
-    seeds = [100]
-    check_amount = 20
+    seeds = list(np.random.randint(100, size=25))
+    p_list = list(np.random.uniform(0, 0.3, size=20))
+    check_amount = 0
     converg_t = 7000
     filename = os.path.join('cnets', 'data', 'saved_data', 'main_dict.pickle')
-    overwrite = True
+    overwrite = False
     if not Path(filename).exists() or overwrite:
-        for seed in seeds:
-            sim_obj = WaveEquationSimulation(store_dict=True)
-            # input = nx.random_lobster(500, 0.3, 0.5, seed=seed)
-            input = gen_strong_graph(seed)
-            graph_in = clone(input)
-            if graph_in is not None:
-                main_dict[str(seed)] = sim_obj.run(graph_in,
-                                                   n=converg_t,
-                                                   checkpoint_amount=check_amount,
-                                                   c=0.3)
+        for p in p_list:
+            for seed in seeds:
+                seed = int(seed)
+                sim_obj = WaveEquationSimulation(store_dict=True)
+                # input = nx.random_lobster(500, 0.3, 0.5, seed=seed)
+                input = gen_strong_graph(seed, p=p)
+                sparse_metric =  (input.number_of_edges() / (input.number_of_nodes()**2))
+                graph_in = clone(input)
+                if graph_in is not None:
+                    main_dict[str(seed) + '_' + str(p)] = {'Simulation Information':
+                                                sim_obj.run(graph_in,
+                                                       n=converg_t,
+                                                       checkpoint_amount=check_amount,
+                                                       c=0.3),
+                                            'Sparsity': sparse_metric,
+                                            'Probability': p,
+                                            'N': input.number_of_nodes()}
         with open(filename, 'wb') as handle:
             pickle.dump(main_dict, handle, pickle.HIGHEST_PROTOCOL)
         print('Saved Model')
     else:
         with open(filename, 'rb') as handle:
             main_dict = pickle.load(handle)
-
-    
-
     # pprint(main_dict)
     plot_from_dict(main_dict)
-    sim_obj.highlight_clusters()
-    sim_obj.draw_partitions()
